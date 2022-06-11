fd5dc618beb18b61a90dd5b15b303047d619ac4c
elissa-c <104518620+elissa-c@users.noreply.github.com>
2022-06-11 14:29:30 +0200

    Update strcpy.asm

--- strcpy.asm
+++ strcpy.asm
@@ -6,15 +6,26 @@ global  main
 extern  printf
 
 section .data
-    format      db 'Hello world!', 0xA, 0
+    	;format      db 'Hello world!', 0xA, 0
+	format_input	db '%1023s', 0
+	format_output	db '%s', 10, 0
 
 section .bss
+	input   resb 1024
+	output  resb 1024
 
 section	.text
 	global	strcpy
 
 strcpy:
 	sub 		rsp, 8
+	
+	lea 		rdi, [format_destination]
+	lea		rsi, [input]
+	mov 		rax, 0
+	call 		scanf wrt ..plt
+	
+	;mov		rcx, 0
 	xor		rcx, rcx
 	jmp		change
 
@@ -29,5 +40,7 @@ change:
 exit:
 	mov     	al, 0
 	call    	printf wrt ..plt
+	
 	mov		rax, rdi
+	add		rsp, 8
 	ret
